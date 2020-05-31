a3fcb0880eb89bbcb0ebdc8b7ab311c3ef9274be
Li-TSE <a0905017579@gmail.com>
2020-05-31 22:58:47 +0800

    2020上機考版本0(李東澤)

---
+++ app/src/main/java/tw/edu/pu/s1071522/trafficlight/GameActivity.java
@@ -0,0 +1,14 @@
+package tw.edu.pu.s1071522.trafficlight;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
